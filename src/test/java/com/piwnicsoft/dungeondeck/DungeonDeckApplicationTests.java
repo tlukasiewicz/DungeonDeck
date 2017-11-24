af1b5260d6357942eccfd662acb97e0644504e77
tlukasiewicz <tomaszlukasiewicz7@gmail.com>
2017-11-24 21:15:06 +0100

    Repository initialization.

---
+++ src/test/java/com/piwnicsoft/dungeondeck/DungeonDeckApplicationTests.java
@@ -0,0 +1,16 @@
+package com.piwnicsoft.dungeondeck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DungeonDeckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
